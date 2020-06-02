ff5c852b119ae2fe87c3c3b1acdff74811600b17
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-06-03 02:40:04 +0900

    Task edit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -69,7 +69,7 @@ class TaskController extends Controller
      */
     public function edit(Task $task)
     {
-        //
+        return view('tasks.edit',compact('task'));
     }
 
     public function update(Request $request, Task $task)
