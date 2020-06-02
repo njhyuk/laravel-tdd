0549b988a017e42dd95ae7a3c93b2ef6231cabed
JunHyuk Noh <junhyuk.noh@kurlycorp.com>
2020-06-03 01:34:27 +0900

    a_user_can_read_all_the_tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::latest()->get();
+
+        return view('tasks.index',compact('tasks'));
     }
 
     /**
